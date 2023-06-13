4b4216083ce40bcc1802dcc99572bf4db362ba2b
idez-kevin <kevincaprino@protonmail.com>
2023-06-13 04:03:04 -0300

    Rotas para os endpoints da api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('cities')->name('api.cities.')->group(function () {
+    Route::get('/', [\App\Http\Controllers\API\CityController::class, 'cities'])->name('cities');
+    Route::get('/details/{id}', [\App\Http\Controllers\API\CityController::class, 'details'])->name('details');
+});
