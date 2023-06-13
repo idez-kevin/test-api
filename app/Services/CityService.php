df0d937f1823613d9fee722f27725a594f926170
idez-kevin <kevincaprino@protonmail.com>
2023-06-13 03:55:42 -0300

    Tratamento de erro e paginacao de municipios

--- app/Services/CityService.php
+++ app/Services/CityService.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Services;
 
+use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Redis;
 
 class CityService {
@@ -16,22 +19,39 @@ class CityService {
         $this->redis = $redis;
     }
 
-    public function AllCities(){
+    public function AllCities($per_page = 30){
         $data = $this->redis::get('cities');
 
         if (!$data) {
-            $response = file_get_contents($this->brasil_api_link);
+            try {
+                $response = file_get_contents($this->brasil_api_link);
+            } catch (Exception $e) {
+                return 'Não foi possível conectar com a BRASIL API!';
+            }
+
             $data = json_decode($response);
 
             $this->redis::set('cities', $response);
             $this->redis::expire('cities', 1800);
-
-            return $data;
+        } else {
+            $data = json_decode($data);
         }
 
-        $cities = json_decode($data);
+        $cities = collect($data);
 
-        return $cities;
+        $current_page = Paginator::resolveCurrentPage('page');
+
+        $paged_data = $cities->slice(($current_page - 1) * $per_page, $per_page)->all();
+
+        $paginated_cities = new LengthAwarePaginator(
+            $paged_data,
+            $cities->count(),
+            $per_page,
+            $current_page,
+            ['path' => Paginator::resolveCurrentPath()]
+        );
+
+        return $paginated_cities;
     }
 
     public function cityDetails($city_id)
